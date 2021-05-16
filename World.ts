943b6df4b83b006dadbfb07c5780ad3e892e7bc3
Hannes Grothkopf <hannes.grothkopf@gmail.com>
2021-05-16 14:56:39 +0200

    autoformat

--- World.ts
+++ World.ts
@@ -3,23 +3,23 @@ export default class World {
     private sizeY: number;
     private grid: Array<Array<number>>; // world tiles are simplified as numbers
 
-    constructor(x: number, y: number){
+    constructor(x: number, y: number) {
         this.sizeX = x;
         this.sizeY = y;
         this.grid = new Array<Array<number>>(x);
-        for(let i = 0; i < x; i++){
+        for (let i = 0; i < x; i++) {
             this.grid[i] = new Array<number>(y).fill(1);
         }
     }
 
-    public setBarriers(barriers: number[][]){
-        for (let i of barriers){
+    public setBarriers(barriers: number[][]) {
+        for (let i of barriers) {
             this.grid[i[0]][i[1]] = 0;
         }
     }
 
-    public isWalkable(x: number, y: number): boolean{        
-        if(x <= this.sizeX && y <= this.sizeY){
+    public isWalkable(x: number, y: number): boolean {
+        if (x <= this.sizeX && y <= this.sizeY) {
             return this.grid[x][y] == 1;
         }
     }
--- app.ts
+++ app.ts
@@ -2,15 +2,15 @@ import Vector2 from './Vector2.js';
 import Unit from './Unit.js';
 import World from './World.js';
 
-let world = new World(10,10);
-world.setBarriers([[2,3],[3,3],[4,3],[4,5],[5,5],[5,6],[5,7]]);
-console.log(`${world.isWalkable(5,4)}`);
-console.log(`${world.isWalkable(5,5)}`);
-console.log(`${world.isWalkable(3,5)}`);
+let world = new World(10, 10);
+world.setBarriers([[2, 3], [3, 3], [4, 3], [4, 5], [5, 5], [5, 6], [5, 7]]);
+console.log(`${world.isWalkable(5, 4)}`);
+console.log(`${world.isWalkable(5, 5)}`);
+console.log(`${world.isWalkable(3, 5)}`);
 
-let player = new Unit(new Vector2(2,8), 'player');
+let player = new Unit(new Vector2(2, 8), 'player');
 console.log(`name: ${player.name} | x: ${player.getPosition().x} | y: ${player.getPosition().y}`);
-let target = new Unit(new Vector2(7,2), 'target');
+let target = new Unit(new Vector2(7, 2), 'target');
 
 console.log(player.getPosition().getLength());
 console.log(target.getPosition().getLength());
